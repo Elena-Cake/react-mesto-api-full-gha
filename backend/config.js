21fb85bd44316afdeb69fac275ea5e1a198a057b
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-03-21 17:40:42 +0300

    change db connect path

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { PORT = '3000' } = process.env;
 const { JWT_SECRET = 'idontknow1secret' } = process.env;
-const { DB_CONNECT_PATH = 'mongodb://0.0.0.0:27017/mestodb' } = process.env;
+const { DB_CONNECT_PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
   PORT,
