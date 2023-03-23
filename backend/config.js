57c2c23fe56cf0f0d6f9745ce7af2bfdfce7f4a4
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-03-23 13:37:11 +0300

    dotenv add

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const { NODE_ENV = 'production' } = process.env;
 const { PORT = '3000' } = process.env;
 const { JWT_SECRET = 'idontknow1secret' } = process.env;
 const { DB_CONNECT_PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
+  NODE_ENV,
   PORT,
   DB_CONNECT_PATH,
   JWT_SECRET,
